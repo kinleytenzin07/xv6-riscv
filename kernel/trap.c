616281dacb741beec251491d33ebc4e931aace9f
Kinley Tenzin <Ktenzin1@miners.utep.edu>
2026-09-24 00:35:26 -0600

    Track per-process CPU time

--- kernel/trap.c
+++ kernel/trap.c
@@ -82,9 +82,10 @@ usertrap(void)
     kexit(-1);
 
   // give up the CPU if this is a timer interrupt.
-  if (which_dev == 2)
+  if (which_dev == 2) {
+    p->cputime++;
     yield();
-
+  }
   prepare_return();
 
   // the user page table to switch to, for trampoline.S
@@ -154,9 +155,10 @@ kerneltrap()
   }
 
   // give up the CPU if this is a timer interrupt.
-  if (which_dev == 2 && myproc() != 0)
+  if (which_dev == 2 && myproc() != 0) {
+    myproc()->cputime++;
     yield();
-
+  }
   // the yield() may have caused some traps to occur,
   // so restore trap registers for use by kernelvec.S's sepc instruction.
   w_sepc(sepc);
